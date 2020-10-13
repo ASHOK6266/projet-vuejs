0ea27b54e0637a0d8498c47de8d3751a3eb921b5
ashokba6266 <ashokba6266@gmail.com>
2020-10-14 01:29:04 +0200

    fetching the product data in the index page

---
+++ vue-app/src/components/IndexPage.vue
@@ -0,0 +1,40 @@
+<template>
+ <div id="index-page">
+
+    <div class="card h-100 d-inline-block" style="width: 18rem;" v-for:="product in products">
+      <img class="card-img-top" :src=product.imgUrl alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">{{product.name}}</h5>
+        <p class="card-text">{{product.price}}</p>
+      </div>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index-page',
+  props: {
+    products: Array,
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
