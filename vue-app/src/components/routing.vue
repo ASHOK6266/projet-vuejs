e26803245dd1eaf74b1d55ac5f9257c6ac50b1cd
ashokba6266 <ashokba6266@gmail.com>
2020-11-13 01:51:42 +0100

    routing

---
+++ vue-app/src/components/routing.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>Hello from routes</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'routingComponent'
+}
+</script>
+
+
+<style>
+/* GLOBAL STYLING */
+#app {
+  background: #202329;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+export default new VueRouter({
+    routes:[
+        {
+            path: '/routes',
+            name: 'route',
+            component: Routing
+        }
+    ]
+})
