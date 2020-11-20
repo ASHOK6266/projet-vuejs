660e2c4559085ce894fb4f22c0cb6adc0077ef26
ashokba6266 <ashokba6266@gmail.com>
2020-11-20 01:17:26 +0100

    starting with the routes

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -3,6 +3,7 @@
   <caroselComponent />
   <index-page :products="products"/>
   <trendingComponent />
+  <router-link to="/product"></router-link>
 </template>
 
 <script>
@@ -17,42 +18,9 @@ export default {
   components: {
     IndexPage,
     caroselComponent,
-    NavbarComponent,
-    TrendingComponent
+    NavbarComponent,  
+    TrendingComponent,
   },
-  data(){
-    return{
-      /*
-      products: [
-          {
-            "imgUrl": "https://guesseu.scene7.com/is/image/GuessEU/M63H24W7JF0-L302-ALTGHOST?wid=1500&fmt=jpeg&qlt=80&op_sharpen=0&op_usm=1.0,1.0,5,0&iccEmbed=0",
-            "name": "CHECK PRINT SHIRT",
-            "price": 110
-          },
-          {
-            "imgUrl": "https://guesseu.scene7.com/is/image/GuessEU/FLGLO4FAL12-BEIBR?wid=700&amp;fmt=jpeg&amp;qlt=80&amp;op_sharpen=0&amp;op_usm=1.0,1.0,5,0&amp;iccEmbed=0",
-            "name": "GLORIA HIGH LOGO SNEAKER",
-            "price": 91
-          },
-          {
-            "imgUrl": "https://guesseu.scene7.com/is/image/GuessEU/HWVG6216060-TAN?wid=700&amp;fmt=jpeg&amp;qlt=80&amp;op_sharpen=0&amp;op_usm=1.0,1.0,5,0&amp;iccEmbed=0",
-            "name": "CATE RIGID BAG",
-            "price": 94.5
-          },
-          {
-            "imgUrl": "http://guesseu.scene7.com/is/image/GuessEU/WC0001FMSWC-G5?wid=520&fmt=jpeg&qlt=80&op_sharpen=0&op_usm=1.0,1.0,5,0&iccEmbed=0",
-            "name": "GUESS CONNECT WATCH",
-            "price": 438.9
-          },
-          {
-            "imgUrl": "https://guesseu.scene7.com/is/image/GuessEU/AW6308VIS03-SAP?wid=700&amp;fmt=jpeg&amp;qlt=80&amp;op_sharpen=0&amp;op_usm=1.0,1.0,5,0&amp;iccEmbed=0",
-            "name": "'70s RETRO GLAM KEFIAH",
-            "price": 20
-          }
-        ]
-        */
-      }
-    }
 }
 </script>
 
@@ -68,3 +36,4 @@ export default {
   margin-top: 60px;
 }
 </style>
+
