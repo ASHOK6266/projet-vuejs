4a0aec74b74308c52ac14b8c0597a0cfabe27631
Valeur Aucibi <valeuraucibi@gmail.com>
2021-01-24 17:18:18 +0100

    refacto front

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,49 +1,46 @@
 <template>
-<div id="app">
-  <Navbar></Navbar>
-
-  <transition name="slide" mode="out-in">
-  <router-view />
-  </transition>
-  <Footer></Footer>
-
-</div>
+    <div id="app">
+        <Navbar></Navbar>
+        <transition name="slide" mode="out-in"><router-view/></transition>
+        <Footer></Footer>
+    </div>
 </template>
 
 <script>
 import Navbar from "./components/views/Navbar.vue"
 import Footer from "./components/views/Footer.vue"
+
 export default {
-  name: 'App',
-  components: {
-    Navbar,
-    Footer
-  },
+    name: 'App',
+    components: {
+        Navbar,
+        Footer
+    },
 }
 </script>
 
 <style>
 @import "assets/css/style.css";
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
 }
 
 a {
-    color: inherit;
+    color: black;
 }
 
 .sli-enter-active,
-.slide-leave-active{
-  transition: opacity 1s, transform 1s;
+.slide-leave-active {
+    transition: opacity 1s, transform 1s;
 }
 
 .sli-enter-active,
-.slide-leave-to{
-  opacity: 0;
-  transform: translateX(-30%);
+.slide-leave-to {
+    opacity: 0;
+    transform: translateX(-30%);
 }
 </style>
 
--- vue-app/src/components/views/Navbar.vue
+++ vue-app/src/components/views/Navbar.vue
@@ -1,102 +1,36 @@
 <template>
-  <!-- HEADER -->
-  <section>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-          <nav class="navbar navbar-expand-lg navbar-light navigation">
+    <!-- HEADER -->
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <nav class="navbar navbar-expand-lg navbar-light navigation">
+                        <a class="navbar-brand"><router-link to="/">IW-Commerce</router-link></a>
 
-            <a class="navbar-brand">
-              <img src="images/logo.png" alt=""> <!--todo : LOGO HERE -->
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav ml-auto main-nav ">
-                <li class="nav-item active">
-                  <a class="nav-link">
-                    <router-link to="/" >
-                      Accueil
-                    </router-link>
-                  </a>
-                </li>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
 
-                <li class="nav-item active">
-                  <a class="nav-link">
-                    <router-link to="/categories" >
-                      Categories
-                    </router-link>
-                  </a>
-                </li>
-
-                <li class="nav-item active">
-                  <a class="nav-link">
-                    <router-link to="/apollo" >
-                      Apollo
-                    </router-link>
-                  </a>
-                </li>
-
-                <li class="nav-item active">
-                  <a class="nav-link">
-                    <router-link to="/vuemik" >
-                      Vuemik test
-                    </router-link>
-                  </a>
-                </li>
-
-                <li class="nav-item active">
-                  <a class="nav-link">
-                    <router-link to="/registration" >
-                      Créer un compte
-                    </router-link>
-                  </a>
-                </li>
-
-
-                <!-- APIs switch
-                <li class="nav-item">
-                  <a class="nav-link"  href="">Api Platform</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link"  href="">GraphQl</a>
-                </li>
-                -->
-
-              </ul>
-              <ul class="navbar-nav ml-auto mt-10">
-                <li class="nav-item">
-                  <a class="nav-link login-button">
-                    <router-link to="/login" >
-                      Se connecter
-                    </router-link>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link login-button">
-                    <router-link to="/register" >
-                      Créer un compte
-                    </router-link>
-                  </a>
-                </li>
-
-                <li class="nav-item">
-                  <a class="nav-link text-white add-button">
-                    <router-link to="/admin" >
-                      <i class="fa fa-plus-circle"></i> Admin
-                    </router-link>
-                  </a>
-                </li>
-              </ul>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav ml-auto main-nav ">
+                                <li class="nav-item"><a class="nav-link"><router-link to="/">Accueil</router-link></a></li>
+                                <li class="nav-item"><a class="nav-link"><router-link to="/categories">Categories</router-link></a></li>
+                                <li class="nav-item"><a class="nav-link"><router-link to="/registration">Créer un compte</router-link></a></li>
+                                <li class="nav-item"><a class="nav-link"><router-link to="/login">Se connecter</router-link></a></li>
+                                <li class="nav-item"><a class="nav-link"><router-link to="/admin">Admin</router-link></a></li>
+                                <!-- APIs switch
+                                <li class="nav-item"><a class="nav-link"  href="">Api Platform</a></li>
+                                <li class="nav-item"><a class="nav-link"  href="">GraphQl</a></li>
+                                -->
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
             </div>
-          </nav>
         </div>
-      </div>
-    </div>
-  </section>
-  <!-- END HEADER -->
+    </section>
+    <!-- END HEADER -->
 </template>
 <script>
 export default {
