2acbebca8babb42dda77d8b926fc67650d91c8d3
Valeur Aucibi <valeuraucibi@gmail.com>
2021-01-26 20:54:02 +0100

    Single page

--- vue-app/src/components/template/IndexPage.vue
+++ vue-app/src/components/template/IndexPage.vue
@@ -69,47 +69,15 @@
                     <!-- offer 01 -->
                     <div class="col-lg-12">
                         <template>
-                            <div v-if="loaded">
-                                <div class="container" v-for="product in products" v-bind:key="product.id">
-                                    <div class="d-flex flex-row ml-3 mb-3" >
-                                        <!-- offer 01 -->
-                                        <div class="col-lg-12">
-                                            <div class="trending-ads-slide">
-                                                <div class="col-sm-12 col-lg-4">
-                                                    <!-- product card -->
-                                                    <div class="product-item bg-light">
-                                                        <div class="card" >
-                                                            <div class="thumb-content">
-                                                                <!-- <div class="price">$200</div> -->
-                                                                <a href="#"> <!-- Faire son routeur avec routeur link -->
-                                                                    <img class="card-img-top img-fluid" :src=product.image alt="Card image cap">
-                                                                </a>
-                                                            </div>
-                                                            <div class="card-body">
-                                                                <h4 class="card-title"><a href="#">{{product.name}}</a></h4>
-                                                                <ul class="list-inline product-meta">
-                                                                    <li class="list-inline-item">
-                                                                        <a href="single.html"><i class="fa fa-folder-open-o"></i>Furnitures</a>
-                                                                    </li>
-                                                                    <li class="list-inline-item">
-                                                                        <a href="#"><i class="fa fa-calendar"></i>26th December</a>
-                                                                    </li>
-                                                                </ul>
-                                                                <p class="card-text">{{product.description}}</p>
-                                                                <div class="product-ratings">
-                                                                    <ul class="list-inline">
-                                                                        <li class="list-inline-item selected"><i class="fa fa-star"></i></li>
-                                                                        <li class="list-inline-item selected"><i class="fa fa-star"></i></li>
-                                                                        <li class="list-inline-item selected"><i class="fa fa-star"></i></li>
-                                                                        <li class="list-inline-item selected"><i class="fa fa-star"></i></li>
-                                                                        <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                                                                    </ul>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
+                            <div class="container">
+                                <div class="row ml-3 mb-3" >
+                                    <div class="card mx-auto mb-3" style="width: 18rem;" v-for="product in products" v-bind:key="product.id">
+                                        <img class="card-img-top" :src=product.image alt="Card image cap">
+                                        <div class="card-body">
+                                            <h5 class="card-title">{{ product.name }}</h5>
+                                            <h6 class="card-subtitle mb-2 text-muted">{{ product.price }} €</h6>
+                                            <p class="card-text">{{ product.description }}</p>
+                                            <a href="/product" class="btn btn-primary">Accéder</a>
                                         </div>
                                     </div>
                                 </div>
@@ -125,25 +93,56 @@
 </template>
 <script>
 
-import ProductsAPI from "@/services/productsAPI";
-
 export default {
     name: 'template-index-page',
-    produit: 'ProductsList',
     data (){
         return {
             loaded: false,
-            products: "",
+            products: [{
+                "id": 1,
+                "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+                "price": 109.95,
+                "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+                "category": "men clothing",
+                "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+            }, {
+                "id": 2,
+                "title": "Mens Casual Premium Slim Fit T-Shirts ",
+                "price": 22.3,
+                "description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+                "category": "men clothing",
+                "image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"
+            }, {
+                "id": 3,
+                "title": "Mens Cotton Jacket",
+                "price": 55.99,
+                "description": "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
+                "category": "men clothing",
+                "image": "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg"
+            }, {
+                "id": 4,
+                "title": "Mens Casual Slim Fit",
+                "price": 15.99,
+                "description": "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
+                "category": "men clothing",
+                "image": "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg"
+            }, {
+                "id": 5,
+                "title": "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
+                "price": 695,
+                "description": "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
+                "category": "jewelery",
+                "image": "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg"
+            }, {
+                "id": 6,
+                "title": "Solid Gold Petite Micropave ",
+                "price": 168,
+                "description": "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
+                "category": "jewelery",
+                "image": "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg"
+            }]
         }
-    },
-    async created() {
-        const prod = await ProductsAPI.findAll() ;
-        if (prod){
-            console.log(prod)
-            this.products = prod
-            this.loaded = true;
-        }
-    },
+    }
 }
 </script>
 <style>
--- vue-app/src/components/views/product.vue
+++ vue-app/src/components/views/product.vue
@@ -1,18 +1,30 @@
 <template>
-  <div>
-    <h1>This is from product route</h1>
-      <ProductsList></ProductsList>
-  </div>
+    <div class="container py-5 my-5 mx-auto border">
+        <div class="row">
+            <div class="col-md-5">
+                <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="Image item" width="400" height="400">
+            </div>
+            <div class="col-md-7">
+                <h3 class="h1">Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</h3>
+                <h5 class="text-secondary">Fourniture</h5>
+                <hr>
+                <h2 class="h2">109.95 €</h2>
+                <p>Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday</p>
+                <a href="#" class="btn btn-raised btn-primary">Acheter</a>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-
-import ProductsList from '../template/ProductsList.vue';
-
 export default {
-  name: "product",
-  components: {
-    ProductsList,
-  }
+    product: {
+        "id": 1,
+        "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+        "price": 109.95,
+        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        "category": "men clothing",
+        "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+    }
 }
 </script>
